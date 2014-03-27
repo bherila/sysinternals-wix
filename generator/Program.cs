ab0f246a0a0caa8e697b3623f3a6a09bba22f90d
Ben Herila <ben@herila.net>
2014-03-27 09:43:51 -0700

    Initial code

---
+++ generator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WixSharp;
+
+namespace generator
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+
+			var project = new Project("CustomActionTest",
+				new Dir(@"%ProgramFiles%\Sysinternals",
+					  new File(@"Files\Docs\Manual.txt"),
+					  new File(@"Files\Bin\MyApp.exe")));
+
+
+			Compiler.BuildWxs(project, "Product.wxs", Compiler.OutputType.MSI);
+
+		}
+	}
+}
